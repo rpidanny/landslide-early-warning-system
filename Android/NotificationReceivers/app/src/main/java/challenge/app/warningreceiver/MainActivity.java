2ba7027db81c4452fbbb2cc6df322ed8e485a404
SubigyaPanta <subigyapanta@gmail.com>
2016-04-23 21:03:19 +0545

    Socket test

--- Android/NotificationReceivers/app/src/main/java/challenge/app/warningreceiver/MainActivity.java
+++ Android/NotificationReceivers/app/src/main/java/challenge/app/warningreceiver/MainActivity.java
@@ -3,11 +3,39 @@ package challenge.app.warningreceiver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class MainActivity extends AppCompatActivity {
 
+    private Socket socket;
+
+    public static final int SERVERPORT = 8000;
+    public static final String SERVERHOST = "192.168.0.56";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread( new ClientThread()).start();
+    }
+
+    class ClientThread implements Runnable{
+        public void run(){
+            try {
+                InetAddress serverAddress = InetAddress.getByName(SERVERHOST);
+                socket = new Socket(serverAddress, SERVERPORT);
+            }
+            catch(UnknownHostException ex){
+                ex.printStackTrace();
+            }
+            catch (IOException ex){
+                ex.printStackTrace();
+            }
+        }
     }
 }
