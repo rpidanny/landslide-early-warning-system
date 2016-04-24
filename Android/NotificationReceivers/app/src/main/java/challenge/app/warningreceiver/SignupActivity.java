e091089ff14d3f138a0d39b14b78b7cdd430cc07
SubigyaPanta <subigyapanta@gmail.com>
2016-04-24 13:48:24 +0545

    khoi k thiyo birsyo

--- Android/NotificationReceivers/app/src/main/java/challenge/app/warningreceiver/SignupActivity.java
+++ Android/NotificationReceivers/app/src/main/java/challenge/app/warningreceiver/SignupActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -26,6 +27,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -133,6 +135,22 @@ public class SignupActivity extends AppCompatActivity implements LoaderCallbacks
         mLocationView = (Spinner) findViewById(R.id.location_list);
         mLocationView.setAdapter(getLocationList());
 
+
+        mLocationView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+               // item = (String) parent.getItemAtPosition(position);
+                if(parent.getChildAt(0)!=null) {
+                    ((TextView) parent.getChildAt(0)).setTextColor(Color.WHITE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         mFullnameView = (EditText) findViewById(R.id.signup_fullname);
         mPhoneView   = (EditText) findViewById(R.id.phone);
 
@@ -144,10 +162,10 @@ public class SignupActivity extends AppCompatActivity implements LoaderCallbacks
     {
         locationList.add("Thapathali");
         locationList.add("Jawalakhel");
-        locationList.add("Gwarko");
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, locationList);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
 
         return adapter;
     }
@@ -385,7 +403,7 @@ public class SignupActivity extends AppCompatActivity implements LoaderCallbacks
      * the user.
      */
     public class UserRegisterTask extends AsyncTask<User, Void, Boolean> {
-        private String requestURL = "http://192.168.1.35:3000/user";
+        private String requestURL = "http://192.168.1.154:3000/user";
         private HashMap<String, String> postDataParams = new HashMap<>();
         private String result = "";
 
@@ -453,8 +471,8 @@ public class SignupActivity extends AppCompatActivity implements LoaderCallbacks
                     //                editor.putString("location", "Thapathali");
                     //                editor.commit();
                     JSONObject response = new JSONObject(result);
-
-                    editor.putString("location", response.getJSONObject("user_data").getString("location"));
+                    int locationIndex = Integer.parseInt(response.getJSONObject("user_data").getString("location"));
+                    editor.putString("location", locationList.get(locationIndex));
 
                     editor.commit();
                     System.out.println(response.getJSONObject("user_data").getString("location"));
